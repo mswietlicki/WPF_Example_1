95f293b2a195ac71f0f1d5ac1d2f451581a97402
Mateusz Świetlicki <mateusz@swietlicki.net>
2014-06-22 12:20:27 +0200

    Added PropertyChanged.Fody and [ImplementPropertyChanged]

--- WPF.Binding/Test/TestViewModel.cs
+++ WPF.Binding/Test/TestViewModel.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PropertyChanged;
 
 namespace WPF.Binding.Test
 {
+    [ImplementPropertyChanged]
     public class TestViewModel
     {
         public TestViewModel()
